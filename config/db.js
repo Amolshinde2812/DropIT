b8d23c42a3e68dd980e4efd74a591fa7d1fdc07c
Amol Baban Shinde <53330037+Amolshinde2812@users.noreply.github.com>
2022-02-27 02:17:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,31 +3,31 @@ const mongoose = require('mongoose');
 
 
 
-// function connectDB() {
-//     // Database connection 
-//     mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-//     const connection = mongoose.connection;
-//     connection.once('open', () => {
-//         console.log('Database connected ');
-//     }).catch(err => {
-//         console.log('Connection failed');
-//     });
-// }
+function connectDB() {
+    // Database connection 
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected ');
+    }).catch(err => {
+        console.log('Connection failed');
+    });
+}
 
 
-const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
-        // useUnifiedTopology: true, <-- no longer necessary
-        // useNewUrlParser: true, <-- no longer necessary
-        // useCreateIndex: true, <-- no longer necessary
-      });
-      console.log(`MongoDB connected :${conn.connection.host}`);
-    } catch (error) {
-      console.error(`error${error}`);
-      process.exit(1);
-    }
-  };
+// const connectDB = async () => {
+//     try {
+//       const conn = await mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+//         // useUnifiedTopology: true, <-- no longer necessary
+//         // useNewUrlParser: true, <-- no longer necessary
+//         // useCreateIndex: true, <-- no longer necessary
+//       });
+//       console.log(`MongoDB connected :${conn.connection.host}`);
+//     } catch (error) {
+//       console.error(`error${error}`);
+//       process.exit(1);
+//     }
+//   };
 
   
 module.exports = connectDB;
