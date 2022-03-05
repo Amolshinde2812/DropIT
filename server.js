523eaa0799d7b80bde12261db5ecf42802743707
Amol Baban Shinde <53330037+Amolshinde2812@users.noreply.github.com>
2022-03-05 16:15:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 //cors
 const corsOptions ={
-    origin:['http//localhost:3000','http//localhost:3000','http//localhost:3000','http://127.0.0.1:3000']
+    origin:['http//localhost:3000','http//localhost:3000','http//localhost:3000','http://127.0.0.1:3000','https://amolshinde2812.github.io/Dropfiles']
 }
 app.use(cors(corsOptions));
 
